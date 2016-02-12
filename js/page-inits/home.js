75261dd8eb261f1714ccdb10102e3a915ccd719e
Arvind <arvsr2002@gmail.com>
2016-02-12 11:29:52 +0530

    Arvind : fixed browserify hbsfy

--- js/page-inits/home.js
+++ js/page-inits/home.js
@@ -1,4 +1,5 @@
 var Handlebars = require("hbsfy/runtime");
+Handlebars.registerPartial('partial1', require('../../views/partials/partial1.hbs'));
 var homeTemplate = require('../../views/partials/para.hbs');
 
 module.exports = {
